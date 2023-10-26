07c93abaea533d096361f38f42a68474045bd67b
Sajid-Bit <68890870+Sajid-Bit@users.noreply.github.com>
2023-10-26 11:28:16 -0700

    fix the error 64

--- src/models/simple_Seq2Seq_model.py
+++ src/models/simple_Seq2Seq_model.py
@@ -8,7 +8,7 @@ from itertools import chain
 import keras
 from keras.regularizers import l2
 from keras.metrics import get
-
+import tensorflow as tf
 
 class Seq2Seq:
     def __init__(self, encoder_layers,
@@ -21,3 +21,23 @@ class Seq2Seq:
         self.output_sequence_length = output_sequence_length  # the number of the output step to predict
         self.decoder_layers = decoder_layers
         self.encoder_layers = encoder_layers
+        self.l2 = l2
+        self._cell_type = cell_type
+
+        self.models = None
+
+        # RNN with the layer of cells
+        def cell():
+            if self.__cell_type == "LSTM":
+                cell = tf.nn.rnn_cell.LSTMCell()
+            elif self.__cell_type == "GRU":
+                cell = tf.nn.rnn_cell.GRUCell()
+            elif self.__cell_type == "RNN":
+                cell = tf.nn.rnn_cell.BasicRNNCell()
+            return cell
+
+            return cell
+
+
+
+
