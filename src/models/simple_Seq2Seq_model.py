9bb44255a8c8ad935a6da1c62d9de0e1ac37e75c
Sajid-Bit <68890870+Sajid-Bit@users.noreply.github.com>
2023-10-28 10:04:39 -0700

    add the method to format the encoder

--- src/models/simple_Seq2Seq_model.py
+++ src/models/simple_Seq2Seq_model.py
@@ -66,3 +66,47 @@ class Seq2Seq:
         for unit in self._encoder_layers:
             # take the h status as input lays
             decoder_state_input_h = Input(shape=(unit,))
+            input_states = []
+
+            if self.cell == LSTMCell:
+                decoder_state_input_c = Input(shape=(unit,))
+                input_states = [decoder_state_input_h, decoder_state_input_c]
+
+            decoder_inputs.extend(input_states)
+
+        return decoder_inputs
+
+    def _format_encoder_states(self, encoder_states, use_first=False):
+        """
+        This method is used to Format the states of encoder block
+        to used only the last state of the of  first layer of the encoder
+
+        """
+        if self.cell == 'LSTM':
+            encoder_states = encoder_states[:2] + [Lambda(lambda x: K.zeros_like(x))(s) for s in encoder_states[2:]]
+
+        else:
+            encoder_states = encoder_states[:1] + [Lambda(lambda x: K.zeros_like(x))(s) for s in encoder_states[1:]]
+
+        return encoder_states
+
+
+class Seq2SeqNet(Seq2Seq):
+
+    def __init__(self, *args, **kwargs):
+        self._decoder_yht = None
+        self._model = None
+        super().__init__(*args, **kwargs)
+
+    def build_model(self, encoder_inputs, decoder_inputs):
+        """
+        -> encoder_inputs : is 3D Tensor the shape is
+        [batch_size, input_sequences, num_features]
+        -> decoder_inputs : is 3D Tensor the shape is
+        [batch_size, output_sequences, num_features]
+
+        """
+        # create the input layers of the encoder and decoders
+        encoder_inputs = Input(shape=encoder_inputs, name='Encoder_Inputs')
+        decoder_inputs = Input(shape=decoder_inputs, name='Decoder_Inputs')
+
