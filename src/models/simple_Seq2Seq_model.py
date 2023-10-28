61426f4950d97ad9b35a00f37621a4b13e4748b4
Sajid-Bit <68890870+Sajid-Bit@users.noreply.github.com>
2023-10-28 04:58:29 -0700

    add the encoder layers

--- src/models/simple_Seq2Seq_model.py
+++ src/models/simple_Seq2Seq_model.py
@@ -18,20 +18,27 @@ class Seq2Seq:
                  units=30,
                  dropout=0.0,
                  l2=0.01,
-                 cell_type='lstm'):
+                 cell_type='LSTM'):
         self._encoder_layers = encoder_layers
         self._decoder_layers = decoder_layers
         self._output_sequence_length = output_sequence_length
 
-        
+        self.l2 = l2
+        if cell_type == 'LSTM':
+            self.cell = LSTMCell
+        elif cell_type == 'GRN':
+            self.cell = GRUCell
+        else:
+            raise ValueError('{0} is not a valid cell type. Choose between gru and lstm.'.format(cell_type))
 
+    def _build_encoder(self):
+        # create the encoder layers by stacked RNN layers
 
+        encoder = []
+        for n_neurons in self._encoder_layers:
+            encoder.append(self.cell(units=n_neurons,
+                                     dropout=self.dropout,
+                                     kernel_regularizer=l2(self.l2),
+                                     recurrent_regularizer=l2(self.l2)))
 
-
-
-
-    def _cell_(self):
-        pass
-
-
-
+        return RNN(encoder, return_state=True, name='encoder-layers')
